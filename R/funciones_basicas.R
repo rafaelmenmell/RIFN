fb6d8236dc492513b6fa56e7cf7c75438e17c7ce
rafael.menendez <rafael.menendez@meteologica.com>
2018-10-19 12:50:43 +0200

    AL descarga ya funciona

--- R/funciones_basicas.R
+++ R/funciones_basicas.R
@@ -4,8 +4,12 @@ Provincias <- function(){
 }
 
 DescargarDatosProvincia <- function(provincia){
+  archivos <- GetArchivos()
   if(is.numeric(provincia)){
-    unzip(zipfile = sprintf("https://www.miteco.gob.es/es/biodiversidad/servicios/banco-datos-naturaleza/ifn3p%01d_tcm30-293907.zip",provincia))
+    file <- archivos[provincia]
+    download.file(url = file,destfile = "temporal.zip",quiet = TRUE,method = "wget",extra = "--no-check-certificate")
+    unzip(zipfile = "temporal.zip",exdir = ".")
+    unlink("temporal.zip")
   } else {
     if (!is.character(provincia)){
       stop("La provincia debe ser un número o una palabra")
@@ -14,7 +18,10 @@ DescargarDatosProvincia <- function(provincia){
       stop("Ese nombre no lo reconozco, si quiere conocer los nombre y códigos teclea Provincias()")
     }
     provincia <- Provincias() %>% dplyr::filter(nombre==provincia) %>% dplyr::select(Codigo) %>% as.numeric()
-    unzip(zipfile = sprintf("https://www.miteco.gob.es/es/biodiversidad/servicios/banco-datos-naturaleza/ifn3p%01d_tcm30-293907.zip",provincia))
+    file <- archivos[provincia]
+    download.file(url = file,destfile = "temporal.zip",quiet = TRUE,method = "wget",extra = "--no-check-certificate")
+    unzip(zipfile = "temporal.zip",exdir = ".")
+    unlink("temporal.zip")
   }
   mdf.file <- list.files(pattern = ".mdb")
   datos <- Hmisc::mdb.get(file = mdf.file)
@@ -23,7 +30,21 @@ DescargarDatosProvincia <- function(provincia){
 }
 
 GetArchivos <- function(){
-  base_page <- "https://www.miteco.gob.es/es/biodiversidad/servicios/banco-datos-naturaleza/informacion-disponible/"
-  h <- httr::handle(base_page)
-  archivos1 <- httr::GET("https://www.miteco.gob.es/es/biodiversidad/servicios/banco-datos-naturaleza/informacion-disponible/ifn3_base_datos_1_25.aspx",handle = h)
+  page1 <- readLines("R/pagina1.html")
+  page1 <- page1[grepl("zip",page1)]
+  page1 <- strsplit(page1,split = "\"")
+  page1 <- page1[grepl("ifn",page1)]
+  page1 <- lapply(page1,function(x) x[grepl("zip",x)])
+  page1 <- lapply(page1,function(x) x[grepl("https",x)])
+  page1 <- unlist(page1)
+  #cutre
+  page2 <- readLines("R/pagina2.html")
+  page2 <- page2[grepl("zip",page2)]
+  page2 <- strsplit(page2,split = "\"")
+  page2 <- page2[grepl("ifn",page2)]
+  page2 <- lapply(page2,function(x) x[grepl("zip",x)])
+  page2 <- lapply(page2,function(x) x[grepl("https",x)])
+  page2 <- unlist(page2)
+  pages <- c(page1,page2)
+  return(pages)
 }
